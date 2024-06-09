0a5e85fef5b9474dd7d29df89ae8898d7cba0618
Urmipie <pablonepikasso8000@gmail.com>
2024-06-09 22:01:05 +0300

    uh oh

--- port.py
+++ port.py
@@ -3,22 +3,21 @@ import serial.tools.list_ports as list_ports
 
 
 class SerialPort(serial.Serial):
-    class SerialPort(serial.Serial):
-        def __init__(self, port=None, speed=9600):
-            if port is None:
-                com_ports = list_ports.comports()
-                if len(com_ports) == 0:
-                    raise ConnectionError("Arduino did not found")
-                elif len(com_ports) == 1:
-                    port = com_ports[0].device
-                else:
-                    print("Choose port:")
-                    for n, port in enumerate(com_ports):
-                        print(f'{n}: {port.device}')
-                    port = com_ports[int(input().strip())].device
-            print(f"Connecting to: {port}")
-            super().__init__(port, speed)
-            print("Connected")
+    def __init__(self, port=None, speed=9600):
+        if port is None:
+            com_ports = list_ports.comports()
+            if len(com_ports) == 0:
+                raise ConnectionError("Arduino did not found")
+            elif len(com_ports) == 1:
+                port = com_ports[0].device
+            else:
+                print("Choose port:")
+                for n, port in enumerate(com_ports):
+                    print(f'{n}: {port.device}')
+                port = com_ports[int(input().strip())].device
+        print(f"Connecting to: {port}")
+        super().__init__(port, speed)
+        print("Connected")
 
     def readline(self, size: any) -> str:
         return super().readline(size).decode("utf-8")
